5c8f284d7a6b065a097ac231dad44cc0801961fd
Michelle <michelle.transbetter@gmail.com>
2024-08-11 11:27:09 +1000

    random number caller

--- resources/js/Pages/Game.jsx
+++ resources/js/Pages/Game.jsx
@@ -1,8 +1,9 @@
-import {  Box, Container, Button, FormControl, FormErrorMessage, FormLabel, Input, Grid, GridItem, VStack, AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, useDisclosure } from '@chakra-ui/react'
+import { Text, Box, Container, Button, FormControl, FormErrorMessage, FormLabel, Input, Grid, GridItem, VStack, AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, useDisclosure } from '@chakra-ui/react'
 import { Field, Form, Formik } from 'formik';
 import { useEffect, useState } from 'react';
 import React from 'react';
 import Share from '../Components/Share';
+import { Inertia } from '@inertiajs/inertia';
 
 function Game() {
   const [isSubmitted, setIsSubmitted] = useState(() => {
@@ -93,154 +94,178 @@ function Game() {
     )
   }
 
-  return (
-    <VStack
-      paddingTop={14}
-      spacing={4}
-      align='center'
-    >
-      <Formik
-          initialValues={{ name: '' }}
-          onSubmit={() => handleSubmit()}
-      >
-        {() => (
-          <Form>
-            <Field name='name' validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Please enter your name</FormLabel>
-                  <Input {...field} placeholder='name' />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme='teal'
-              type='submit'
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-      {isSubmitted &&
-        <VStack
-          spacing={4}
-          align='stretch'
-        >
-          <Container maxW='md' bg='tomato' color='white'>
-            <Box padding='4' color='tomato' maxW='md'>
-              <Grid templateColumns='repeat(5, 1fr)' gap={2}>
-                <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>B</GridItem>
-                <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>I</GridItem>
-                <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>N</GridItem>
-                <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>G</GridItem>
-                <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>O</GridItem>
-              </Grid>
-              {isGenerated ?
-                <Grid templateColumns='repeat(5, 1fr)' gap={1}>
-                  {arr.slice(0,12).map((index) =>
-                    <GridItem key={index} w='100%' h='10' bg='white' >
-                      <Button onClick={(e) => { handleMark(index, e) }} color={markedButton === index ? 'gray' : ''} border='0px' variant='ghost' fontSize='20px'>{index}</Button>
-                    </GridItem>
-                  )}
-                  <GridItem w='100%' h='10' bg='white' color='tomato'>
-                    <Button color='tomato' border='0px' variant='ghost' fontSize='10px'>FREE</Button>
-                  </GridItem>
-                  {arr.slice(12,24).map((index) =>
-                    <GridItem key={index} w='100%' h='10' bg='white' >
-                      <Button onClick={(e) => { handleMark(index, e) }} color={markedButton === index ? 'gray' : ''} border='0px' variant='ghost' fontSize='20px'>{index}</Button>
-                    </GridItem>
-                  )}
-                </Grid>
-                : <Grid templateColumns='repeat(5, 1fr)' gap={1}>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' color='tomato'>
-                    <Button color='tomato' border='0px' variant='ghost' fontSize='10px'>FREE</Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                  <GridItem w='100%' h='10' bg='white' >
-                      <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
-                  </GridItem>
-                </Grid>
-              }
-            </Box>
-          </Container>
-          {!!markedButton ? <Alert />
-            : <Button onClick={() => handleGenerate()} colorScheme='blue'>Generate</Button>
-          }
-          <Share />
-        </VStack>
+  function fetchRandomNumber() {
+    fetch('/game', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
       }
-    </VStack>
+    })
+    .then(response => response.json())
+      .then(data => {
+        document.getElementById('random-number').innerText = data.number;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+  return (
+    <>
+      <VStack
+        paddingTop={14}
+        spacing={4}
+        align='center'
+      >
+        <Formik
+            initialValues={{ name: '' }}
+            onSubmit={() => handleSubmit()}
+        >
+          {() => (
+            <Form>
+              <Field name='name' validate={validateName}>
+                {({ field, form }) => (
+                  <FormControl isInvalid={form.errors.name && form.touched.name}>
+                    <FormLabel>Please enter your name</FormLabel>
+                    <Input {...field} placeholder='name' />
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Button
+                mt={4}
+                colorScheme='teal'
+                type='submit'
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        {isSubmitted &&
+          <VStack
+            spacing={4}
+            align='stretch'
+          >
+            <Container maxW='md' bg='tomato' color='white'>
+              <Box padding='4' color='tomato' maxW='md'>
+                <Grid templateColumns='repeat(5, 1fr)' gap={2}>
+                  <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>B</GridItem>
+                  <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>I</GridItem>
+                  <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>N</GridItem>
+                  <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>G</GridItem>
+                  <GridItem fontSize='30px' w='100%' h='10' bg='tomato' color='white'>O</GridItem>
+                </Grid>
+                {isGenerated ?
+                  <Grid templateColumns='repeat(5, 1fr)' gap={1}>
+                    {arr.slice(0,12).map((index) =>
+                      <GridItem key={index} w='100%' h='10' bg='white' >
+                        <Button onClick={(e) => { handleMark(index, e) }} color={markedButton === index ? 'gray' : ''} border='0px' variant='ghost' fontSize='20px'>{index}</Button>
+                      </GridItem>
+                    )}
+                    <GridItem w='100%' h='10' bg='white' color='tomato'>
+                      <Button color='tomato' border='0px' variant='ghost' fontSize='10px'>FREE</Button>
+                    </GridItem>
+                    {arr.slice(12,24).map((index) =>
+                      <GridItem key={index} w='100%' h='10' bg='white' >
+                        <Button onClick={(e) => { handleMark(index, e) }} color={markedButton === index ? 'gray' : ''} border='0px' variant='ghost' fontSize='20px'>{index}</Button>
+                      </GridItem>
+                    )}
+                  </Grid>
+                  : <Grid templateColumns='repeat(5, 1fr)' gap={1}>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' color='tomato'>
+                      <Button color='tomato' border='0px' variant='ghost' fontSize='10px'>FREE</Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                    <GridItem w='100%' h='10' bg='white' >
+                        <Button color='tomato' border='0px' variant='ghost' fontSize='20px'></Button>
+                    </GridItem>
+                  </Grid>
+                }
+              </Box>
+            </Container>
+            {!!markedButton ? <Alert />
+              : <Button onClick={() => handleGenerate()} colorScheme='blue'>Generate</Button>
+            }
+            <Share />
+          </VStack>
+        }
+      </VStack>
+      <VStack>
+        <form action="" method="post">
+          <Button onClick={() => fetchRandomNumber()} colorScheme='yellow'>Call next number</Button>
+        </form>
+        <Text id="random-number"></Text>
+      </VStack>
+    </>
   )
 }
 
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/game', [GameController::class, 'index'])->name('game.index');
+    Route::match(['get', 'post'], '/game', [GameController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
